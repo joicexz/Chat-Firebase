921774c7be476847873b8544b9903c740af86525
Joice Soares de Almeida <joicesoaresalmeida@gmail.com>
2025-05-30 10:39:44 -0300

    30-05

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,7 +9,13 @@ import { getFirestore, collection } from 'firebase/firestore'
 // 3. Crie um web app e copie o firebseConfigs abaixo 
 
 const firebaseConfig = {
-
+    apiKey: "AIzaSyArEV0mwyV_jrNGf2IeMvZRl0344CvhJv4",
+    authDomain: "ddmii-bd1a5.firebaseapp.com",
+    projectId: "ddmii-bd1a5",
+    storageBucket: "ddmii-bd1a5.firebasestorage.app",
+    messagingSenderId: "253176901434",
+    appId: "1:253176901434:web:35accfaabdc1369c0f6a03",
+    measurementId: "G-YST27Z6X4K"
 };
 
 // Initialize Firebase
